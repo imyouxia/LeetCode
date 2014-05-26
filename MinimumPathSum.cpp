a8f5fbeb4d44d846f3bba6def9c6171028e7105c
armsword <root@linuxer.me>
2014-05-26 19:53:13 +0800

    路径和-DP

---
+++ MinimumPathSum.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+	int minPathSum(vector<vector<int> > &grid)
+	{
+		if(grid.empty() || grid[0].empty())
+			return 0;
+		int m = grid.size();
+		int n = grid[0].size();
+		vector< vector<int> >dp(m,vector<int>(n,0));
+		int i,j;
+		for(i = 0; i < m; i++)
+		{
+			for(j = 0; j < n; j++)
+			{
+				if(i == 0 && j == 0)
+					dp[i][j] = grid[i][j];
+				else if(i == 0)
+					dp[i][j] = dp[i][j-1] + grid[i][j];
+				else if(j == 0)
+					dp[i][j] = dp[i - 1][j] + grid[i][j];
+				else
+					dp[i][j] = min(dp[i - 1][j],dp[i][j - 1]) + grid[i][j];
+			}
+		
+		}
+		return dp[m - 1][n - 1];
+	}
+};
+
+int main()
+{
+
+}
